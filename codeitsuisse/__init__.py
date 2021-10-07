6beb6b1eab6eafb708d306bac11a9e0b1626f933
气味 <njycgqw@icloud.com>
2021-10-07 21:40:08 +0800

    add assignment to heroku

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,5 +3,6 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.ttt
 import codeitsuisse.routes.test
+import codeitsuisse.routes.Assignment_2_7
 
 
---
+++ codeitsuisse/routes/Assignment_2_7.py
@@ -0,0 +1,29 @@
+import logging
+import json
+from flask import request, jsonify
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/assignment_2_7', methods=['POST'])
+def main():
+    heap=[22,18,20,17,3,16,14,7,12,19,9]
+    
+    def minheap(heap,i):
+        left=2*i+1
+        right=2*i+2
+        if left<len(heap) and heap[left]<heap[i]:
+            small=left
+        else:
+            small=i
+        if right<len(heap) and heap[right]<heap[small]:
+            small=right
+        if small!=i:
+            heap[small],heap[i] = heap[i],heap[small]
+            minheap(heap,small)
+
+    n = int(len(heap)//2-1)
+    for i in range(n,-1,-1):
+        minheap(heap,i)
+    
+    print(heap)
