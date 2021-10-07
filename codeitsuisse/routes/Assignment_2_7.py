5d46e7776c1117872347b0d4b0979c2def00c2d8
气味 <njycgqw@icloud.com>
2021-10-07 21:51:37 +0800

    Update Assignment_2_7.py

--- codeitsuisse/routes/Assignment_2_7.py
+++ codeitsuisse/routes/Assignment_2_7.py
@@ -7,12 +7,10 @@ logger = logging.getLogger(__name__)
 
 @app.route('/assignment_2_7', methods=['POST'])
 def main():
-    try:
-        data = request.get_json()
-        logging.info("data sent for evaluation {}".format(data))
-        heap = data.get("input")
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    heap = data.get("input")
     # heap=[22,18,20,17,3,16,14,7,12,19,9]
-    
     def minheap(heap,i):
         left=2*i+1
         right=2*i+2
@@ -30,4 +28,4 @@ def main():
     for i in range(n,-1,-1):
         minheap(heap,i)
     logging.info("My result :{}".format(heap))
-    return json.dumps(result)
+    return json.dumps(heap)
