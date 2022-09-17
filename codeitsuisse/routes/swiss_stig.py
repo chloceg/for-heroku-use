3221b6b89c3b179b9ebf52fdb43b7b00d0bd1781
气味 <njycgqw@icloud.com>
2022-09-18 00:05:03 +0800

    Update swiss_stig.py

--- codeitsuisse/routes/swiss_stig.py
+++ codeitsuisse/routes/swiss_stig.py
@@ -9,14 +9,14 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/stig/warmup', methods=['POST'])
-def stigwarmup():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
+def swiss_stig_warmup():
+    raw = request.get_json()
+    logging.info("data sent is {}".format(raw))
     result = []
-    for i in data:
+    for i in raw:
         result.append(stig(i['questions'],i['maxRating']))
     
-    logging.info("result: {}".format(result))
+    logging.info("my result is {}".format(result))
     return jsonify(result)
 
 def stig(questions,maxRating):
@@ -25,11 +25,11 @@ def stig(questions,maxRating):
     for i in range(1,maxRating+1):
         pg = list(range(1,maxRating+1))
         for q in questions:
-            if (i >= q["lower"] and i <= q["upper"]):
-                pg = [x for x in pg if x >= q["lower"] and x <= q["upper"]]
+            if q["lower"] <= i <= q["upper"]:
+                out = [x for x in pg if x >= q["lower"] and x <= q["upper"]]
             else:
-                pg = [x for x in pg if x < q["lower"] or x > q["upper"]]
-        if (i == min(pg)):
+                out = [x for x in pg if x < q["lower"] or x > q["upper"]]
+        if (i == min(out)):
             count += 1
     d = math.gcd(count, maxRating)
     return {"p": count // d, "q": maxRating // d}
