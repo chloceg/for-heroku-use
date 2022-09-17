d47702c351d4fdc36f16237a410b8e1ce65e9042
气味 <njycgqw@icloud.com>
2022-09-17 13:33:08 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -11,27 +11,29 @@ logger = logging.getLogger(__name__)
 def max_num():
     res = 0
     output = []
+    d = {}
     raw = request.get_json()
     logging.info("data sent is {}".format(raw))
     nums = raw
     for entry in nums: # entry = [6,7,8,9,10]
         temp = []
         for item in entry:
-            logging.info("every num is {}".format(item))
             if item == 1 or item == 2:
                 temp.append(4)
                 continue
             x = item
             res = x
             while x != 1:
-                if x % 2 == 1:
+                if x in d:
+                    break
+                if x&1 == 1:
                     x = 3 * x + 1
-                elif x % 2 == 0:
-                    x = int(x/2)
-                res = max(res, x)
-                # print(res)
+                    res = max(res, x)
+                else:
+                    x = x // 2
+                if x == 1:
+                    d[item] = res
             temp.append(res)
-        logging.info("test for each of the list {}".format(temp))
         output.append(temp)
     r = output
     logging.info("My crypto result :{}".format(r))
