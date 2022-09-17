2346db732d35d52c9128ee5e567809726c4ecef3
气味 <njycgqw@icloud.com>
2022-09-17 21:56:30 +0800

    Update swiss_stig.py

--- codeitsuisse/routes/swiss_stig.py
+++ codeitsuisse/routes/swiss_stig.py
@@ -10,17 +10,20 @@ logger = logging.getLogger(__name__)
 
 @app.route('/stig/warmup', methods=['POST'])
 def swiss_stig():
+    out = []
     raw = request.get_json()
     logging.info("data sent is {}".format(raw))
-    questions = raw["questions"]
-    maxrating = raw["maxRating"]
-    lower = questions[0]["lower"]
-    upper = questions[0]["upper"]
-    p = upper - lower + 1
-    q = maxrating
-    p //= math.gcd(maxrating, upper-lower+1)
-    q //= math.gcd(maxrating, upper-lower+1)
-    d = {}
-    d["p"], d["q"] = p, q
-    logging.info("my result dict s {}".format(d))
-    return json.dumps(d)
+    questions = raw[0]["questions"]
+    maxrating = raw[0]["maxRating"]
+    for i in range(len(questions)):
+        lower = questions[i]["lower"]
+        upper = questions[i]["upper"]
+        p = upper - lower + 1
+        q = maxrating
+        p //= math.gcd(maxrating, upper-lower+1)
+        q //= math.gcd(maxrating, upper-lower+1)
+        d = {}
+        d["p"], d["q"] = p, q
+        out.append(d)
+    logging.info("my result dict s {}".format(out))
+    return json.dumps(out)
