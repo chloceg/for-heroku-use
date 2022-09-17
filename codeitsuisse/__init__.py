f60c873e01805c7e44c3020dc126220b3d63954b
气味 <njycgqw@icloud.com>
2022-09-17 10:34:33 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-import codeitsuisse.routes.square
-# import codeitsuisse.routes.ttt
-# import codeitsuisse.routes.test
-# import codeitsuisse.routes.Assignment_2_7
 
+
+import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
 import codeitsuisse.routes.tickerStreamPart2
 import codeitsuisse.routes.cryptocollapz
